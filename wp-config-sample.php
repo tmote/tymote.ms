e8ba88616b95544a77a15232eedac965006adca5
Ty Mote <tymote@microsoft.com>
2018-09-17 15:47:37 -0500

    Fix redirects temp

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -18,6 +18,9 @@
  * @package WordPress
  */
 
+define('WP_HOME','http://tymote.ms');
+define('WP_SITEURL','http://tymote.ms');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'database_name_here');
