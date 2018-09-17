eb5eafecb0386a9c844acad73e05bf810d04b098
Ty Mote <tymote@microsoft.com>
2018-09-17 13:56:35 -0500

    Hard coded database strings

--- wp-config.php
+++ wp-config.php
@@ -36,25 +36,23 @@ foreach ($_SERVER as $key => $value) {
 
 
 // ** MySQL settings - You can get this info from your web host ** //
-/*
 /** The name of the database for WordPress */
-define('DB_NAME', $connectstr_dbname);
+#define('DB_NAME', $connectstr_dbname);
 
 /** MySQL database username */
-define('DB_USER', $connectstr_dbusername);
+#define('DB_USER', $connectstr_dbusername);
 
 /** MySQL database password */
-define('DB_PASSWORD', $connectstr_dbpassword);
+#define('DB_PASSWORD', $connectstr_dbpassword);
 
 /** MySQL hostname */
-define('DB_HOST', $connectstr_dbhost);
+#define('DB_HOST', $connectstr_dbhost);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-*/
 
 /** Testing direct connection strings for DB **/
 /** The name of the database for WordPress */
